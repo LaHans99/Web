3a926cacbca4fa0acd0bd1ed5548732cc678ca53
LaHans99 <43384927+LaHans99@users.noreply.github.com>
2022-03-24 17:27:50 -0600

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,27 @@
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Challengers'
+    }
+});
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        bucl:[
+            { message:'SRT'  },
+            { message:'HellCat' },
+            { message:'Demon' },
+        ]
+    }
+  })
+
+ var app3 = new Vue({
+    el: '#modelo',
+    data:{
+        name: 'Carro Challenger',
+        dt: '800,000',
+        imag:'https://www.dodge.com.mx/content/uploads/2018/11/dodge-challenger-2019-galeria-exterior-04-832x520.jpg'
+    },
+
+});
